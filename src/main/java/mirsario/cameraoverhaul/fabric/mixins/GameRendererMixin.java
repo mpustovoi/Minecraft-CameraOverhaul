0cecbd1f2a87882fcb41d69aee2007b9cbef606f
Mirsario <me@mirsar.io>
2024-05-08 22:28:02 +0300

    Switch to Mojang mappings (ridiculously they're more consistent).

--- src/main/java/mirsario/cameraoverhaul/fabric/mixins/GameRendererMixin.java
+++ src/main/java/mirsario/cameraoverhaul/fabric/mixins/GameRendererMixin.java
@@ -4,24 +4,26 @@ package mirsario.cameraoverhaul.fabric.mixins;
 import mirsario.cameraoverhaul.abstractions.*;
 import mirsario.cameraoverhaul.callbacks.*;
 import mirsario.cameraoverhaul.structures.*;
-import net.minecraft.client.render.*;
-import net.minecraft.client.util.math.*;
-import net.minecraft.util.math.*;
+import net.minecraft.client.*;
+import net.minecraft.client.renderer.*;
+import net.minecraft.world.phys.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
+import com.mojang.blaze3d.vertex.*;
 
 @Mixin(GameRenderer.class)
+@SuppressWarnings("UnusedMixin")
 public abstract class GameRendererMixin
 {
-	@Shadow @Final private Camera camera;
+	@Shadow @Final private Camera mainCamera;
 
-	@Inject(method = {"tiltViewWhenHurt", "bobViewWhenHurt"}, at = @At("HEAD"))
-	private void PostCameraUpdate(MatrixStack matrices, float f, CallbackInfo ci)
+	@Inject(method = "bobHurt", at = @At("HEAD"))
+	private void PostCameraUpdate(PoseStack matrices, float f, CallbackInfo ci)
 	{
-		Transform cameraTransform = new Transform(camera.getPos(), new Vec3d(camera.getPitch(), camera.getYaw(), 0d));
+		Transform cameraTransform = new Transform(mainCamera.getPosition(), new Vec3(mainCamera.getXRot(), mainCamera.getYRot(), 0));
 
-		cameraTransform = ModifyCameraTransformCallback.EVENT.Invoker().ModifyCameraTransform(camera, cameraTransform);
+		cameraTransform = ModifyCameraTransformCallback.EVENT.Invoker().ModifyCameraTransform(mainCamera, cameraTransform);
 
 		MathAbstractions.RotateMatrixByAxis(matrices, 0f, 0f, 1f, (float)cameraTransform.eulerRot.z);
 	}
