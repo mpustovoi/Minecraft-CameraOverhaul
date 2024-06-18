3bf5271e6d4a96fa1302820219e8214d99310e46
Samuel McCarthy <110861054+sam-mccarthy@users.noreply.github.com>
2024-06-18 08:27:32 -0400

    Update mixin and gradle. (#55)
    * Update gradle
    
    * Replaced mixin with more portable version
    
    * Fix View Bobbing dependence.
    
    * Changelog entry + whitespace fix.
    
    ---------
    
    Co-authored-by: sam-mccarthy <samuelmccarthy03@gmail.com>
    Co-authored-by: Mirsario <me@mirsar.io>

--- src/main/java/mirsario/cameraoverhaul/fabric/mixins/modern/GameRendererMixin.java
+++ src/main/java/mirsario/cameraoverhaul/fabric/mixins/modern/GameRendererMixin.java
@@ -17,19 +17,13 @@ public abstract class GameRendererMixin
 	@Final
 	private Camera camera;
 
-	@Inject(method = "renderWorld", at = @At(
-		value = "INVOKE",
-		// Inject before the call to Camera.update()
-		target = "Lnet/minecraft/client/render/Camera;update(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;ZZF)V",
-		shift = At.Shift.BEFORE
-	))
-	private void PostCameraUpdate(float tickDelta, long limitTime, MatrixStack matrix, CallbackInfo ci)
+	@Inject(method = {"tiltViewWhenHurt", "bobViewWhenHurt"}, at = @At("HEAD"))
+	private void PostCameraUpdate(MatrixStack matrices, float f, CallbackInfo ci)
 	{
 		Transform cameraTransform = new Transform(camera.getPos(), new Vec3d(camera.getPitch(), camera.getYaw(), 0d));
 
 		cameraTransform = ModifyCameraTransformCallback.EVENT.Invoker().ModifyCameraTransform(camera, cameraTransform);
 
-		//matrix.multiply(Vec3f.POSITIVE_Z.getDegreesQuaternion((float)cameraTransform.eulerRot.z));
-		MathAbstractions.RotateMatrixByAxis(matrix, 0f, 0f, 1f, (float)cameraTransform.eulerRot.z);
+		MathAbstractions.RotateMatrixByAxis(matrices, 0f, 0f, 1f, (float)cameraTransform.eulerRot.z);
 	}
 }
